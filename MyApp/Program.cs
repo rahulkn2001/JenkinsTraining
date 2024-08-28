03497a49c20be5f3e1e1b83f1e8fe7b463675f00
Rahul Krishna Nair <rahul.nair@planview.com>
2024-08-28 12:07:43 +0530

    initial commit

---
+++ MyApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyApp;
+
+Console.WriteLine("Hello, World!");
+
+
+Calculator obj = new Calculator();
+Console.WriteLine(obj.Add(2, 3));
